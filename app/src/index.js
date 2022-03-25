6b4301ad29d666b8ef8530aefda9eb1c294939fa
karolkew <karolke99@gmail.com>
2022-03-25 20:49:03 +0100

    initial react app added and config update

--- app/src/index.js
+++ app/src/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from '../src/components/Form';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-/**
- * @config preparing config prop.
- * api: url for the server endpoint
- * title: form title
- * successMessage: message will show in the UI when mail is successfully sent.
- * errorMessage: message will show in the UI when mail is not sent.
- * fields: this is the name of each field. This should be exact order of the fieldsConfig and fieldsConfig.fieldName should be  the same
- * fieldsConfig = settings for each input/textarea field
- */
-const config = {
-  api: `${process.env.REACT_APP_API}`,
-  title: 'Contact Me',
-  description: 'Write us about your request hehehehehe, We\'ll get back to you within 24 hours.',
-  successMessage: 'Thank you for contacting me.',
-  errorMessage: 'Please fill the complete form',
-  fields:{
-    firstName: '',
-    lastName: '',
-    email: '',
-    msg: ''
-  },
-  fieldsConfig:  [
-   { id: 1, label: 'First Name', fieldName: 'firstName', type: 'text',placeholder:'Your First Name', isRequired: true , klassName:'first-name-field'},
-   { id: 2, label: 'Last Name', fieldName: 'lastName', type: 'text', placeholder: 'Your Last Name', isRequired: true , klassName:'last-name-field'},
-   { id: 3, label: 'Email', fieldName: 'email', type: 'email', placeholder: ' Your Email', isRequired: true , klassName:'email-field'},
-   { id: 4, label: 'Message', fieldName: 'msg', type: 'textarea',placeholder:'Write something.....', isRequired: true , klassName:'message-field'}
-  ]
-}
-ReactDOM.render(<Form config={config} />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
