60166b09fc4dcf5e0ef5d607541f601639d87d71
Amadeusz <amadeuszwalag@gmail.com>
2022-04-05 23:13:04 +0200

    basic button

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,9 @@
 import React, {Component} from 'react'
 
-import logo from './logo.svg';
-import './App.css';
-
-import TestApiConnection from './TestApiConnection';
-
 class App extends Component {
   render() {
     return <div className="App">
-        <h1>Hello form React</h1>
+
     </div>
   };
 }
