cb191da02b435735ce36375bb8c836f7e4c626c7
Amadeusz <amadeuszwalag@gmail.com>
2022-04-06 20:10:47 +0200

    init test

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
 
+import LoginView from './Views/LoginView'
+
 class App extends Component {
   render() {
-    return <div className="App">
-
+    return <div style={{margin: 0}} className="App">
+        <LoginView />
+        
     </div>
   };
 }
---
+++ app/src/Views/LoginView.js
@@ -0,0 +1,10 @@
+import '../Css/LoginView.css'
+
+const LoginView = () =>
+{
+    return <div className = "LoginView">
+            <p1>Hello</p1>
+        </div>
+}
+
+export default LoginView
