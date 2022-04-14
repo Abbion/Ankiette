9feadc28a7fc72556fe5be85e4f5cc44184b8a12
Amadeusz <amadeuszwalag@gmail.com>
2022-04-14 22:03:13 +0200

    View finished ready for refactor

--- app/src/Components/FontImporter.js
+++ app/src/Components/FontImporter.js
@@ -4,9 +4,9 @@
 const FontImporter = () => {
 
     return <div className='Fonts'>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200&display=swap" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200&display=swap" rel="stylesheet"/>
             </div>
 }
 
