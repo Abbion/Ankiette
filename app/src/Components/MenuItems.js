67d734a80170b5be3cc0084a56b095baf282a76b
madziarq <magdalena.zolnierek@gmail.com>
2022-04-25 01:11:30 +0200

    Navbar's first version

---
+++ app/src/Components/MenuItems.js
@@ -0,0 +1,19 @@
+import '../Css/BasicComponents.css'
+
+export const MenuItems = [
+    <button className="Button">
+        Your forms
+    </button>,
+
+    <button className="Button">
+        Create new form
+    </button>,
+
+    <button className="Button">
+        Reports
+    </button>,
+
+    <button className="Button">
+        Account
+    </button>
+]
