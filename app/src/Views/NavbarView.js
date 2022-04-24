f8d473916ed5da7ec5fdded9e80ac7955f134261
madziarq <magdalena.zolnierek@gmail.com>
2022-04-25 01:11:54 +0200

    Created Navbar's view for testing

---
+++ app/src/Views/NavbarView.js
@@ -0,0 +1,11 @@
+import Navbar from '../Components/Navbar'
+import "../Css/NavbarView.css"
+
+const NavbarView = () =>
+{
+    return <div className="NavbarView">
+        <Navbar />
+    </div>
+}
+
+export default NavbarView
