683e44898446375a9bc35673a74c1d0810768107
madziarq <magdalena.zolnierek@gmail.com>
2022-05-07 21:49:04 +0200

    Navbar attempt #2
    Adjusted scalling, added icons (still don't work idk why), tried to add drop-down menu but not successfully.

--- app/src/Components/MenuItems.js
+++ app/src/Components/MenuItems.js
@@ -2,18 +2,22 @@ import '../Css/BasicComponents.css'
 
 export const MenuItems = [
     <button className="Button">
-        Your forms
+        <a onClick={()=> {
+            window.location.href = 'app/public/index.html'
+        }}>
+            Your forms
+        </a>
     </button>,
 
-    <button className="Button">
+    <button className="Button" url='app/public/index.html'>
         Create new form
     </button>,
 
-    <button className="Button">
+    <button className="Button" url='#'>
         Reports
     </button>,
 
-    <button className="Button">
+    <button className="Button" url='#'>
         Account
     </button>
 ]
--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -3,12 +3,21 @@ import { MenuItems } from "./MenuItems"
 import '../Css/Navbar.css'
 
 class Navbar extends Component {
+    state = {clicked: false }
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
             <nav className="NavbarItems">
-                <ul className="nav-menu">
+                <ul className={this.state.clicked ? "nav-menu-active" : "nav-menu"}>
                     {MenuItems.map((button, index) => React.cloneElement(button, {key: index}))}
                 </ul>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <img src={this.state.clicked ? "../Svg/MenuBars.svg" : "../../public/Graphics/Icon/MenuXmark.png"}></img>
+                </div>
             </nav>
         )
     }
