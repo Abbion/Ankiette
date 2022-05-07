de741cde27f626252a132ef5a658f0a40dea3dbb
madziarq <magdalena.zolnierek@gmail.com>
2022-05-07 21:49:04 +0200

    Navbar attempt #2
    Adjusted scalling, added icons (still don't work idk why), tried to add drop-down menu but not successfully.

--- app/src/Views/NavbarView.js
+++ app/src/Views/NavbarView.js
@@ -1,10 +1,16 @@
 import Navbar from '../Components/Navbar'
 import "../Css/NavbarView.css"
+import PreLoginTemplateView from './PreLoginTemplateView'
 
 const NavbarView = () =>
 {
     return <div className="NavbarView">
-        <Navbar />
+        <div className="Navbar">
+            <Navbar />
+        </div>
+        <div className="ContentView">
+            <PreLoginTemplateView />
+        </div>
     </div>
 }
 
