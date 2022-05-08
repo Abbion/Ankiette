54ca55832b1aeed9415a95ab9e83bc80e423ad21
Karol <karolke99@gmail.com>
2022-05-08 22:50:15 +0200

    rebase fixes

---
+++ app/src/Views/LoginView.js
@@ -0,0 +1,19 @@
+import '../Css/TemplateView.css'
+import LoginComponent from '../Components/LoginComponent'
+
+
+const LoginView = () =>
+{
+    return <div className = "TemplateView">
+        <div className = "LeftSide">
+            <div className = "CatchPhrase">
+                Tick your data in one place with Ankiette
+            </div>
+        </div>
+        <div className = "RightSide">
+            <LoginComponent />
+        </div>
+    </div>
+}
+
+export default LoginView;
