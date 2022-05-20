01af2f8d7c2b0c0e8e395e6d8e16ce662930c9fa
Amadeusz <amadeuszwalag@gmail.com>
2022-05-21 01:36:26 +0200

    Layout done

---
+++ app/src/Components/AddQuestionComponent.js
@@ -0,0 +1,46 @@
+import '../Css/CreateForm.css'
+import '../Css/BasicComponents.css'
+import { useState } from 'react';
+
+const AddQuestionComponent = () =>
+{
+    const [questionArr, setQuestionArr] = useState(['1']);
+
+    function onAddQuestionClicked(){
+        console.log("Add question");
+
+        setQuestionArr(prevState =>(
+            {
+                questionArr : [...prevState, '2']
+            }
+        ));
+        console.log(questionArr);
+    }
+
+    return <div className = "AddQuestionComponent">
+        <div className="AddAndConfigm">
+            <h1 className="FormName">
+                My new form
+            </h1>
+
+            <div className="Questions">
+                {questionArr}
+            </div>
+
+            <button className="Button" onClick={onAddQuestionClicked}>
+                <h2>+</h2>
+                <h1>Add question</h1>
+            </button>
+
+            <h1 className="ConfirmInfo">
+                Confirm and create new form - Your form will be saved and You will be able to share it.
+            </h1>
+
+            <button className="Button ConfirmButton">
+                <h1>Confirm</h1>
+            </button>
+        </div>
+    </div>
+}
+
+export default AddQuestionComponent
--- app/src/Views/CreateNewFormView.js
+++ app/src/Views/CreateNewFormView.js
@@ -1,22 +1,12 @@
 import '../Css/CreateForm.css'
 import Navbar from '../Components/Navbar'
+import AddQuestionComponent from '../Components/AddQuestionComponent'
 
 const CreateForm = () =>
 {
     return <div className = "CreateForm">
         <Navbar/>
-        <div className="AddAndConfigm">
-            <button>
-                <h1>Add question</h1>
-            </button>
-
-            <h1>
-                Confirm and create new form - Your form will be saved and You will be able to share it.
-            </h1>
-            <button>
-                Confirm
-            </button>
-        </div>
+        <AddQuestionComponent/>
     </div>
 }
 
